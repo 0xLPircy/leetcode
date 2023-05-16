ab1cd2397992a1cfb1e28bc3cc5611daf38678e2
L-Pircy <taveesha.agarwal@gmail.com>
2023-05-16 12:55:58 +0530

    1.TwoSum fixed final

--- 1.TwoSum.c
+++ 1.TwoSum.c
@@ -1,21 +1,16 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int *twoSum(int *nums, int numsSize, int target, int *returnSize)
-{
-    int ans[2], ans1 = 0, ans2 = 0;
-    for (int i = 0; i < numsSize; i++)
-    {
-        for (int j = i + 1; j < numsSize; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                ans1 = i;
-                ans2 = j;
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    *returnSize=2;
+    int *ans = malloc(2*(sizeof(int)));
+    for (int i=0;i<numsSize; i++){
+        for (int j=i+1; j<numsSize; j++){
+            if (nums[i]+nums[j]==target){
+    ans[0]=i;
+    ans[1]=j;
             }
         }
     }
-    ans[0] = ans1;
-    ans[1] = ans2;
     return ans;
 }
